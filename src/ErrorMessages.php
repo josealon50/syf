18e75c6776bc35612e9a09ec3e85900a6c13021d
Jose Alonso Leon <jleon@sandiegobloodbank.org>
2020-11-22 20:15:29 -0800

    Added constant error messages classes

--- src/ErrorMessages.php
+++ src/ErrorMessages.php
@@ -1,7 +1,7 @@
 <?php
     class ErrorMessages{
-        public $PROMO_CD_EXC_ERR='PROMO CODE ERROR'; 
-        public $INVALID_PROMO_CD_ERROR='INVALID PROMO CODE'; 
+        const PROMO_CD_EXC_ERR='PROMO CODE ERROR'; 
+        const INVALID_PROMO_CD_ERROR='INVALID PROMO CODE'; 
 
 
     }
