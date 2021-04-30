1ff103caa30fef7b6258c91e7300a7a005b6b4a8
Jose Alonso Leon <jleon@sandiegobloodbank.org>
2021-04-29 22:33:43 -0700

    Check if store code is null we can remove it from the where clause

--- db/ASPRecon.php
+++ db/ASPRecon.php
@@ -37,7 +37,11 @@ class ASPRecon extends IDBTable {
     public function isRecordProcessed( $record ){
         global $appconfig;
 
-        $where = "WHERE AS_CD = '" . $record['AS_CD'] . "' AND AMT = '" . $record['AMT'] . "' AND AS_STORE_CD = '" . $record['ORIGIN_STORE'] . "' AND IVC_CD = '" . $record['DEL_DOC_NUM'] . "' ";
+        $where = "WHERE AS_CD = '" . $record['AS_CD'] . "' AND AMT = '" . $record['AMT'] . "' AND AS_STORE_CD = '" . $record['ORIGIN_STORE'] . "' ";
+        
+        if( $record['DEL_DOC_NUM'] !== '' ){ 
+            $where .= "' AND IVC_CD = '" . $record['DEL_DOC_NUM'] . "' ";
+        }
 
         $result = $this->query($where);
         if( $row = $this->next() ){
